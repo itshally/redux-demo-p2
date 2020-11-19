41dfa8ba04b9374c71e8c7ef6084710ef7b705ec
Hally <hbianan@gmail.com>
2020-11-19 13:52:43 -0500

    files updated

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -6,7 +6,7 @@
 
 import React from 'react'
 import { connect } from 'react-redux'
-import { buyCake } from './redux'
+import { buyCake } from '../redux'
 
 function CakeContainer(props) {
     return (
--- src/components/HooksCakeContainer.js
+++ src/components/HooksCakeContainer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { buyCake } from './redux'
+import { buyCake } from '../redux'
 
 function HooksCakeContainer() {
 
